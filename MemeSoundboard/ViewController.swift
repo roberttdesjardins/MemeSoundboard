af8553cf9bfb98f53b802e28eee0b7e868a5f13d
Robert Desjardins <robd@Roberts-MBP.hitronhub.home>
2018-01-22 15:30:59 -0800

    Before any new view controllers

---
+++ MemeSoundboard/ViewController.swift
@@ -0,0 +1,112 @@
+//
+//  ViewController.swift
+//  MemeSoundboard
+//
+//  Created by Robert Desjardins on 2018-01-17.
+//  Copyright © 2018 Robert Desjardins. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func doYouKnowDaWaeBtnPressed(_ sender: Any) {
+        let path = Bundle.main.path(forResource: "doyouknowdawae", ofType: "wav")!
+        let url = URL(fileURLWithPath: path)
+        do {
+            player = try AVAudioPlayer(contentsOf: url)
+            player.prepareToPlay()
+        } catch let error as NSError {
+            print(error.description)
+        }
+        player.play()
+    }
+    
+    @IBAction func smokeWeedBtnPressed(_ sender: Any) {
+        let path = Bundle.main.path(forResource: "smokeweed", ofType: "wav")!
+        let url = URL(fileURLWithPath: path)
+        do {
+            player = try AVAudioPlayer(contentsOf: url)
+            player.prepareToPlay()
+        } catch let error as NSError {
+            print(error.description)
+        }
+        player.play()
+    }
+    
+    @IBAction func snoopDogBtnPressed(_ sender: Any) {
+        let path = Bundle.main.path(forResource: "holdup", ofType: "wav")!
+        let url = URL(fileURLWithPath: path)
+        do {
+            player = try AVAudioPlayer(contentsOf: url)
+            player.prepareToPlay()
+        } catch let error as NSError {
+            print(error.description)
+        }
+        player.play()
+    }
+    
+    @IBAction func omaeBtnPressed(_ sender: Any) {
+        let path = Bundle.main.path(forResource: "omae", ofType: "wav")!
+        let url = URL(fileURLWithPath: path)
+        do {
+            player = try AVAudioPlayer(contentsOf: url)
+            player.prepareToPlay()
+        } catch let error as NSError {
+            print(error.description)
+        }
+        player.play()
+    }
+    
+    @IBAction func naniBtnPressed(_ sender: Any) {
+        let path = Bundle.main.path(forResource: "nani", ofType: "wav")!
+        let url = URL(fileURLWithPath: path)
+        do {
+            player = try AVAudioPlayer(contentsOf: url)
+            player.prepareToPlay()
+        } catch let error as NSError {
+            print(error.description)
+        }
+        player.play()
+    }
+    
+    @IBAction func prankedBtnPressed(_ sender: Any) {
+        let path = Bundle.main.path(forResource: "pranked", ofType: "wav")!
+        let url = URL(fileURLWithPath: path)
+        do {
+            player = try AVAudioPlayer(contentsOf: url)
+            player.prepareToPlay()
+        } catch let error as NSError {
+            print(error.description)
+        }
+        player.play()
+    }
+    
+    @IBAction func thugLifeBtnPressed(_ sender: Any) {
+        let path = Bundle.main.path(forResource: "thuglife", ofType: "wav")!
+        let url = URL(fileURLWithPath: path)
+        do {
+            player = try AVAudioPlayer(contentsOf: url)
+            player.prepareToPlay()
+        } catch let error as NSError {
+            print(error.description)
+        }
+        player.play()
+    }
+    
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
