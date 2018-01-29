4bc7ba1b80e7fafaa2d91ab28c1456616dce4b1e
Robert Desjardins <robd@Roberts-MBP.hitronhub.home>
2018-01-29 14:17:30 -0800

    Added 2 more buttons. Last commit before trying ads

--- MemeSoundboard/Controller/Screen3VC.swift
+++ MemeSoundboard/Controller/Screen3VC.swift
@@ -16,6 +16,30 @@ class Screen3VC: UIViewController {
         super.viewDidLoad()
         
     }
+    
+    @IBAction func ohshitBtnPressed(_ sender: Any) {
+        let path = Bundle.main.path(forResource: "ohshit", ofType: "wav")!
+        let url = URL(fileURLWithPath: path)
+        do {
+            player = try AVAudioPlayer(contentsOf: url)
+            player.prepareToPlay()
+        } catch let error as NSError {
+            print(error.description)
+        }
+        player.play()
+    }
+    @IBAction func noiceBtnPressed(_ sender: Any) {
+        let path = Bundle.main.path(forResource: "noice", ofType: "wav")!
+        let url = URL(fileURLWithPath: path)
+        do {
+            player = try AVAudioPlayer(contentsOf: url)
+            player.prepareToPlay()
+        } catch let error as NSError {
+            print(error.description)
+        }
+        player.play()
+    }
+    
  
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
